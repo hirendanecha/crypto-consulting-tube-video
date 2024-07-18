762297ba34bbc88df9d6a0031a8bd91fea4e405a
Swapnil Opash <sra.opash@gmail.com>
2024-07-18 10:55:03 +0530

    changed conference link

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -19,7 +19,7 @@ export const environment = {
   domain: '.cryptoconsulting.tube',
   wasabiUrl: wasabiUrl,
   socketUrl: `${backendUrl}`,
-  conferenceUrl: 'https://facetime.tube/',
+  conferenceUrl: 'https://meet.facetime.tube/',
   logoutUrl: logoutUrl
 };
 
